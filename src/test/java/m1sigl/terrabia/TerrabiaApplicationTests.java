682adc4a9db564700f0c814678337d9aff9328f8
emilekmdDev <emilekamdem.c@gmail.com>
2025-11-25 23:48:51 +0100

    first commit

---
+++ src/test/java/m1sigl/terrabia/TerrabiaApplicationTests.java
@@ -0,0 +1,13 @@
+package m1sigl.terrabia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrabiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
